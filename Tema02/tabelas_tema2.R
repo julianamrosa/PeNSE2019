b63b602cdae3ea35d53decb1bc41daa7dbec8c37
julianamrosa <47371910+julianamrosa@users.noreply.github.com>
2023-09-01 12:09:45 -0300

    Add files via upload

--- Tema02/tabelas_tema2.R
+++ Tema02/tabelas_tema2.R
@@ -259,7 +259,8 @@ tabela_222_pt4 <- modelo2(DESENHO=subset(desenho_validos, (pense$SABE!="Abandono
                           NOM_VAR_LIN="Dependência Administrativa",
                           VETOR_LIN="Privada")
 
-tabela_222 <- rbind(tabela_222_pt1, tabela_222_pt2, tabela_222_pt3, tabela_222_pt4)
+tabela_222 <- rbind(tabela_222_pt1, tabela_222_pt2,
+                    tabela_222_pt3, tabela_222_pt4)
 
 wb <- loadWorkbook("Tema02/Tema02.xlsx")
 addWorksheet(wb,"2.2.2")
@@ -323,4 +324,371 @@ tabela_223 <- rbind(tabela_223_pt1, tabela_223_pt2, tabela_223_pt3, tabela_223_p
 wb <- loadWorkbook("Tema02/Tema02.xlsx")
 addWorksheet(wb,"2.2.3")
 writeData(wb,"2.2.3",tabela_223)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+## TABELA 2.3.1 ##
+
+table(pense$ENTENDE) #pais entendem seus problemas e preocupações
+
+tabela_231_pt1 <- modelo1(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono"
+                                                           & pense$B01001A==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Sexo",
+                          VALOR_EXTRA="Masculino")
+
+tabela_231_pt2 <- modelo1(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono"
+                                                           & pense$B01001A==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Sexo",
+                          VALOR_EXTRA="Feminino")
+
+tabela_231_pt3 <- modelo1(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono"
+                                                           & pense$DEP_ADMIN==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Dependência Administrativa",
+                          VALOR_EXTRA="Pública")
+
+tabela_231_pt4 <- modelo1(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono"
+                                                           & pense$DEP_ADMIN==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Dependência Administrativa",
+                          VALOR_EXTRA="Privada")
+
+tabela_231 <- rbind(tabela_231_pt1, tabela_231_pt2,
+                    tabela_231_pt3, tabela_231_pt4)
+
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.3.1")
+writeData(wb,"2.3.1",tabela_231)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+## TABELA 2.3.2 ##
+
+table(pense$ENTENDE) #pais sabem o que o aluno faz no tempo livre
+
+tabela_232_pt1 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$B01001A==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Masculino")
+
+tabela_232_pt2 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$B01001A==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Feminino")
+
+tabela_232_pt3 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Pública")
+
+tabela_232_pt4 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Privada")
+
+tabela_232 <- rbind(tabela_232_pt1, tabela_232_pt2, tabela_232_pt3,
+                    tabela_232_pt4)
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.3.2")
+writeData(wb,"2.3.2",tabela_232)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+## TABELA 2.3.3 ##
+
+table(pense$ENTENDE) #pais entendem seus problemas e preocupações
+
+tabela_233_pt1 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$B01001A==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Masculino")
+
+tabela_233_pt2 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$B01001A==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Feminino")
+
+tabela_233_pt3 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$DEP_ADMIN==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Pública")
+
+tabela_233_pt4 <- modelo3(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Privada")
+
+tabela_233 <- rbind(tabela_233_pt1, tabela_233_pt2, tabela_233_pt3,
+                    tabela_233_pt4)
+
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.3.3")
+writeData(wb,"2.3.3",tabela_233)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## TABELA 2.4.1 ##
+#TEM ALGO ERRADO
+table(pense$PRESTATIVOS) #colegas foram prestativos/ trataram bem
+
+tabela_241_pt1 <- modelo1(DESENHO=subset(desenho_validos, (pense$PRESTATIVOS!="Abandono"
+                                                           & pense$B01001A==1)),
+                          VAR_COL="PRESTATIVOS",
+                          NOME_VAR_COL="Frequência com que seus colegas foram prestativos/ te trataram bem",
+                          VETOR_COL=prestativos,
+                          FILTRO=2:4,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Sexo",
+                          VALOR_EXTRA="Masculino")
+
+tabela_241_pt2 <- modelo1(DESENHO=subset(desenho_validos, (pense$PRESTATIVOS!="Abandono"
+                                                           & pense$B01001A==2)),
+                          VAR_COL="PRESTATIVOS",
+                          NOME_VAR_COL="Frequência com que seus colegas foram prestativos/ te trataram bem",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Sexo",
+                          VALOR_EXTRA="Feminino")
+
+tabela_241_pt3 <- modelo1(DESENHO=subset(desenho_validos, (pense$PRESTATIVOS!="Abandono"
+                                                           & pense$DEP_ADMIN==1)),
+                          VAR_COL="PRESTATIVOS",
+                          NOME_VAR_COL="Frequência com que seus colegas foram prestativos/ te trataram bem",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Dependência Administrativa",
+                          VALOR_EXTRA="Pública")
+
+tabela_241_pt4 <- modelo1(DESENHO=subset(desenho_validos, (pense$PRESTATIVOS!="Abandono"
+                                                           & pense$DEP_ADMIN==2)),
+                          VAR_COL="PRESTATIVOS",
+                          NOME_VAR_COL="Frequência com que seus colegas foram prestativos/ te trataram bem",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_4vars",
+                          VAR_EXTRA="Dependência Administrativa",
+                          VALOR_EXTRA="Privada")
+
+tabela_241 <- rbind(tabela_241_pt1, tabela_241_pt2,
+                    tabela_241_pt3, tabela_241_pt4)
+
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.4.1")
+writeData(wb,"2.4.1",tabela_241)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+## TABELA 2.3.2 ##
+
+table(pense$ENTENDE) #pais sabem o que o aluno faz no tempo livre
+
+tabela_232_pt1 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$B01001A==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Frequência com que seus colegas foram prestativos/ te trataram bem",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Masculino")
+
+tabela_232_pt2 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$B01001A==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Feminino")
+
+tabela_232_pt3 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Pública")
+
+tabela_232_pt4 <- modelo2(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Privada")
+
+tabela_232 <- rbind(tabela_232_pt1, tabela_232_pt2, tabela_232_pt3,
+                    tabela_232_pt4)
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.3.2")
+writeData(wb,"2.3.2",tabela_232)
+saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
+
+
+
+## TABELA 2.3.3 ##
+
+table(pense$ENTENDE) #pais entendem seus problemas e preocupações
+
+tabela_233_pt1 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$B01001A==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Masculino")
+
+tabela_233_pt2 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$B01001A==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Sexo",
+                          VETOR_LIN="Feminino")
+
+tabela_233_pt3 <- modelo3(DESENHO=subset(desenho_validos,
+                                         (pense$ENTENDE!="Abandono"
+                                          & pense$DEP_ADMIN==1)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Pública")
+
+tabela_233_pt4 <- modelo3(DESENHO=subset(desenho_validos, (pense$ENTENDE!="Abandono" & pense$DEP_ADMIN==2)),
+                          VAR_COL="ENTENDE",
+                          NOME_VAR_COL="Seu responsável entende seus problemas/preocupações?",
+                          VETOR_COL="Sim",
+                          FILTRO=2,
+                          fun_estima="estima_pct",
+                          fun_arruma="tab_3vars",
+                          NOM_VAR_LIN="Dependência Administrativa",
+                          VETOR_LIN="Privada")
+
+tabela_233 <- rbind(tabela_233_pt1, tabela_233_pt2, tabela_233_pt3,
+                    tabela_233_pt4)
+
+wb <- loadWorkbook("Tema02/Tema02.xlsx")
+addWorksheet(wb,"2.3.3")
+writeData(wb,"2.3.3",tabela_233)
 saveWorkbook(wb,"Tema02/Tema02.xlsx",overwrite = TRUE)
